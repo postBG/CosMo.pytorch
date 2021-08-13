1fab1b7a93b96059fc28684fa217b14a24c891ca
Dongwan <dongwan123@gmail.com>
2021-08-13 15:11:57 +0900

    Fix LSTM hidden size bug

--- models/text_encoders/__init__.py
+++ models/text_encoders/__init__.py
@@ -9,16 +9,20 @@ def text_encoder_factory(vocabulary: AbstractBaseVocabulary, config: dict) -> Ab
     model_code = config['text_encoder']
     feature_size = config['text_feature_size']
     word_embedding_size = config['word_embedding_size']
+    lstm_hidden_size = config['lstm_hidden_size']
 
     if model_code == SimpleLSTMEncoder.code():
         return SimpleLSTMEncoder(vocabulary_len=len(vocabulary), padding_idx=vocabulary.pad_id(),
-                                 feature_size=feature_size, word_embedding_size=word_embedding_size)
+                                 feature_size=feature_size, word_embedding_size=word_embedding_size,
+                                 lstm_hidden_size=lstm_hidden_size)
     elif model_code == NormalizationLSTMEncoder.code():
         return NormalizationLSTMEncoder(vocabulary_len=len(vocabulary), padding_idx=vocabulary.pad_id(),
                                         feature_size=feature_size, norm_scale=config['norm_scale'],
-                                        word_embedding_size=word_embedding_size)
+                                        word_embedding_size=word_embedding_size,
+                                        lstm_hidden_size=lstm_hidden_size)
     elif model_code == SimplerLSTMEncoder.code():
         return SimplerLSTMEncoder(vocabulary_len=len(vocabulary), padding_idx=vocabulary.pad_id(),
-                                  feature_size=feature_size, word_embedding_size=word_embedding_size)
+                                  feature_size=feature_size, word_embedding_size=word_embedding_size,
+                                  lstm_hidden_size=lstm_hidden_size)
     else:
         raise ValueError("There's no text encoder matched with {}".format(model_code))
